3baaeb4fa95bf976d9eac3192ab3c6b3f39d6dfc
Sonja Leaf <sonjaqleaf@gmail.com>
2020-04-24 18:05:31 -0500

    updates draw speed to max 60fps

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ sock.connect('tcp://eddn.edcd.io:9500');
 console.log('Worker connected to EDDN Firehose');
 
 sock.subscribe('');
-
+let screenContent = { lastMessage: undefined, counters: undefined, maxLocation: undefined, lastLocation: undefined, topLocations: undefined };
 let maxLocation = { "system": "SQL", "station": ".media", "sellPrice": 0, "timestamp": new Date()};
 let maxes = [];
 let counters = { req: 0, commoditiesReq: 0, lastReqTime: undefined, ltdCount:0 };
@@ -18,65 +18,92 @@ sock.on('message', topic => {
   let theCounters = getCounters();
   theCounters.req++;
   let content = JSON.parse(zlib.inflateSync(topic));
-  theCounters.lastReqTime = moment(content.message.timestamp).fromNow();
+  theCounters.lastReqTime = moment(content.message.timestamp).fromNow(true);
   if ( content['$schemaRef'] === 'https://eddn.edcd.io/schemas/commodity/3' ) { 
     theCounters.commoditiesReq++;
     let commodities = content.message.commodities;	
     let system = content.message.systemName;
     let station = content.message.stationName;
-    let msg = { text: "none", lastStation: system + " " + station };
+    let msg = system + " " + station;
     commodities.forEach( item  => { 
       
       if ( item.name == "lowtemperaturediamond" ) { 
-	      console.clear();
         getCounters().ltdCount++;
-	      let thisItem = { "system": system,  "station": station, "sellPrice": item.sellPrice, "timestamp": new Date(content.message.timestamp) };
+	      let thisItem = { 
+						system: system,
+						station: station, 
+            sellPrice: item.sellPrice, 
+            timestamp: new Date(content.message.timestamp), 
+						humanTime: moment(this.timestamp).fromNow() };
 	      let maxLoc = getMaxLocation();
         if ( thisItem.sellPrice > maxLoc.sellPrice ) {
-          console.log( "NEW MAX PRICE!!!!" );
+          msg = "NEW MAX PRICE!!!!\n" + msg;
           updateMaxLocation(thisItem);
         } else {
 	        updateMaxLocations( thisItem );
         }
 	
-
-	      maxLoc = getMaxLocation();
-	      let tableKeys = ["system", "station", "sellPrice"];
-				msg.text = JSON.stringify( maxLoc ); 
-	      console.table( [counters], ['req', 'commoditiesReq', 'lastReqTime', 'ltdCount'] );
-        console.log("Latest LTD Location: ", moment(thisItem.timestamp).fromNow(true));
-        console.table([thisItem], tableKeys);
-        console.log("MAX Location: ", moment(maxLoc.timestamp).fromNow(true) );
-        console.table([maxLoc], tableKeys);
-        console.log("Captured Locations");
-        console.table(getMaxes().slice(0,60), tableKeys);
+				screenContent.lastLocation = thisItem;
+	      screenContent.maxLocation = getMaxLocation();
+				screenContent.topLocations = getMaxes();
       } else { 
-	getCounters().otherCount++;
+	       getCounters().otherCount++;
       }
     }); 
+		screenContent.lastMessage = msg;
   } 
-
+ drawScreen();
 });
+
+
+let lastFrame = Date.now();
+const frameThrottle = 17; // 17ms is just under 60fps
+function drawScreen() {
+	frameTime = Date.now() - lastFrame;
+	if ( frameTime < frameThrottle ) return;
+	lastFrame = frameTime;	
+  let tableKeys = ["system", "station", "sellPrice"];
+	console.clear();
+	screenContent.counters = getCounters();
+  screenContent.topLocations = getMaxes().slice(0,50);
+	console.table( [screenContent.counters], ['req', 'commoditiesReq', 'lastReqTime', 'ltdCount'] );
+	if ( screenContent.lastMessage !== undefined ) console.log(screenContent.lastMessage);
+  if ( screenContent.lastLocation) { 
+		console.log("Latest LTD Location: ", moment(screenContent.lastLocation.timestamp).fromNow(true));
+  	console.table([screenContent.lastLocation], tableKeys);
+  }
+	if (screenContent.maxLocation) {
+    console.log("MAX Location: ", moment(screenContent.maxLocation.timestamp).fromNow(true) );
+    console.table([screenContent.maxLocation], tableKeys);
+  }  
+
+	if (screenContent.topLocations) { 
+    console.log("Captured Locations");
+    console.table(screenContent.topLocations, tableKeys);
+  }
+}
 
 function getCounters() { return counters };
 function getMaxLocation() { return getMaxes()[0]; }
 
-function updateMaxLocation ( location ) {
-  updateMaxLocations( location );
+function updateMaxLocation ( loc ) {
+  updateMaxLocations( loc );
   maxLocation = getMaxLocation();
 }
 
 function updateMaxLocations( target ) {
   const theMaxes = getMaxes();
-  const sysMatch = theMaxes.findIndex( (e) => e.system === target.system );
-  const stationMatch = theMaxes.findIndex( (e) => e.station === target.station );
-  if (sysMatch === -1 ) {
+  const stationMatch = theMaxes.findIndex( (e) => e.station === target.station && e.system === target.system );
+  if ( stationMatch > -1 ) {
+    if ( theMaxes[stationMatch].timestamp >= target.timestamp ) {
+    	theMaxes[stationMatch] = target;
+    	screenContent.lastMessage = target.system + " " + target.station + " updated!";
+    } else {
+			screenContent.lastMessage = "Old data for: " + target.system + " " + target.station;
+		}
+  } else {
     maxes.push(target);
-  } else if ( sysMatch !== stationMatch ) {
-    maxes.push(target);
-  } else if ( sysMatch === stationMatch && theMaxes[sysMatch].timestamp < target.timestamp) {
-    maxes[stationMatch] = target;
-    console.log( target.system + " updated!" );
+    screenContent.lastMessage = target.system + " " + target.station + " added!";
   }
 }
 
